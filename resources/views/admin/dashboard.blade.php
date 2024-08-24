11b2a12e2ffb3c7be334dec9f06b872d02c8f296
Hammad Khan <hammadkhanhk152@gmail.com>
2024-08-24 17:32:20 +0500

    initial setup welcome and adnin panel

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.admin')
+@section('content')
+    <main id="main" class="main">
+        <div class="pagetitle">
+            <h1>Dashboard</h1>
+            <nav>
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                    <li class="breadcrumb-item active">Dashboard</li>
+                </ol>
+            </nav>
+        </div>
+        <!-- End Page Title -->
+
+        <h2>hiiiiiiii</h2>
+
+
+
+    </main>
+    <!-- End #main -->
+@endsection
